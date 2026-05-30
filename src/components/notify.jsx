55caaceec95676c510fdc4986dfd13753e77b4af
Coder Nelly <codernelly34@gmail.com>
2026-05-30 12:11:45 +0100

    Enhance Notify component to check for notifyText

--- src/components/notify.jsx
+++ src/components/notify.jsx
@@ -7,14 +7,14 @@ export const Notify = () => {
   const { notifyText, notify, setNotify } = useNotify();
 
   useEffect(() => {
-    if (!notify) return;
+    if (!notify || !notifyText) return;
     const timer = setTimeout(() => setNotify(false), 1500);
     return () => clearTimeout(timer);
   }, [notify]);
 
   return (
     <div
-      className={`fixed z-9999 text-center ${notify ? "top-1.5" : "-top-20"} bg-[#e2eaec] flex justify-self-center rounded py-2 px-4 transition-all duration-200 ease-linear shadow shadow-yellow-50 font-semibold`}
+      className={`fixed z-9999 text-center ${notify && notifyText? "top-1.5" : "-top-20"} bg-[#e2eaec] flex justify-self-center rounded py-2 px-4 transition-all duration-200 ease-linear shadow shadow-yellow-50 font-semibold`}
     >
       {notifyText}
     </div>
