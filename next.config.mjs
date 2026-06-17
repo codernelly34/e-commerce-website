f6250f0893e0c84d025c9b1ad319da46f074ff4b
Coder Nelly <codernelly34@gmail.com>
2026-06-17 13:29:19 +0100

    feat: add allowedDevOrigins configuration to nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: JSON.parse(process.env.allowedDevOrigins),
+};
 
 export default nextConfig;
